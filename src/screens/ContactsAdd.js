b29fe967f0f2ac376c7326e4f60e901332193ab0
eduardaaires <eduardaaires@yahoo.com.br>
2020-09-01 20:08:18 -0300

    Adicao de input na tela de contatos

--- src/screens/ContactsAdd.js
+++ src/screens/ContactsAdd.js
@@ -1,14 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import {Text, View } from 'react-native';
+import {Text, View ,TextInput} from 'react-native';
 import styles from '../styles/styles';
 
 export default class ContactsAdd extends React.Component {
     render(){
       return (
         <View style={styles.container}>
-          <Text>Contatos!</Text>
-          <StatusBar style="auto" />
+          <TextInput style={styles.txtInput}/>
+          {/* <Text>Contatos!</Text> */}
+          {/* <StatusBar style="auto" /> */}
         </View>
       );
       }
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -8,6 +8,15 @@ const styles = StyleSheet.create({
       alignItems: 'center',
       justifyContent: 'center',
     },
+    txtInput: {
+      width:280,
+      height: 40, 
+      borderColor: 'gray', 
+      borderWidth: 1 ,
+      borderRadius: 20,
+      marginBottom: 20, 
+      fontSize: 18
+    }
   });
 
   export default styles;
