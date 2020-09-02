9418ecc0b27161de9d65f1885b34b7321d6fccd2
Rafael Alexandre <rafael.unifei.eng@gmail.com>
2020-09-02 19:53:31 -0300

    Adicionei tela Login e conectei com firebase

--- App.js
+++ App.js
@@ -3,9 +3,33 @@ import { createAppContainer } from 'react-navigation';
 
 import Home from './src/screens/Home';                
 import ContactsAdd from './src/screens/ContactsAdd';
+import Login from './src/screens/Login';
+
+import * as firebase from 'firebase';
+
+
+//Initialize firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD1fdB4jtUUWaO97zXjsWROW5uQPt7csaU",
+  authDomain: "react-native-firebase-94486.firebaseapp.com",
+  databaseURL: "https://react-native-firebase-94486.firebaseio.com",
+  projectId: "react-native-firebase-94486",
+  storageBucket: "react-native-firebase-94486.appspot.com",
+};
+
+
+
+  firebase.initializeApp(firebaseConfig);
+
 
 //criacao da stack de navegacao entre telas
 const Nav = createStackNavigator({
+  Login:{
+    screen:Login,
+    navigationOptions:{
+      title:'Login'
+    }
+  },
   Home:{    
     screen:Home,
     navigationOptions:{
@@ -18,6 +42,7 @@ const Nav = createStackNavigator({
       title:'Contatos'
     }
   }
+  
 })
 
 //adiciona a stack em um container
